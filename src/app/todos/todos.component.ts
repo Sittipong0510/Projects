82ae06e3925a9b095edaf6bc19d2c18c1d931c3a
Sittipong0510 <sittipong0510@gmail.com>
2021-12-12 08:05:25 +0700

    first commit

---
+++ src/app/todos/todos.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { DataService } from '../shared/data.service';
+import { Todo } from '../shared/todo.model';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.scss']
+})
+export class TodosComponent implements OnInit {
+  
+  todos: Todo[] = [];
+  showValidationErrors:boolean | undefined
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+    this.todos = this.dataService.getAllTodos()
+  }
+
+  onFormSubmit(form:NgForm){
+    if (form.invalid) {
+      this.showValidationErrors = true
+      return
+    }
+
+    this.dataService.addTodo(new Todo(form.value.text))
+    this.showValidationErrors = false
+    form.reset()
+  }
+
+  toggleCompleted(todo: Todo){
+    todo.completed = !todo.completed;
+  }
+
+  editTodo(todo: Todo){
+    // const index = this.todos?.indexOf(todo)
+    // this.dataService.updateTodo()
+  }
+
+  deleteTodo(todo: Todo){
+    const index = this.todos.indexOf(todo)
+    this.dataService.deleteTodo(index)
+  }
+
+}
